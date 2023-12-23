983d8078f7730bea51e4d49fae6f20512508818c
Reaper <kmr.arnav3484@gmail.com>
2023-12-23 18:57:17 +0530

    Added Ideas.md and updated main.cpp
    The ideas.md is for sharing and collaborating on ideas and also keeping track of tasks. Although I believe it can also be done through git somehow, but I don't know how yet. Also added more content in the main.cpp file and restructured it. There is some work to be done now. I'll be working on the Input function and the player class.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,57 @@
 #include <iostream>
 #include <stdio.h>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+// - R means Reaper's comment
+// - Y means Yeager's comment
+
+// In this project for dyanamic arrays we will use vector arrays which
+// have automatic memory management making them easier to work with
+// instead of new and delete[]
+
+
+// Spaceship Design
+// Bullet design
+
+
+// Enemy design
+// Bullet design
+
+
+
+void drawGameWindow();
+// Give this a try - R
+
+
+void takeInput(); 
+
+
+class Player 
+{
+    int playerHealth = 5;
+
+};
+
+// Work on this class as well - R
+class Enemy 
+{
+    int enemyHealth = 1;
+};
+
 
 int main()
 {
+    // Main game loop
+    bool running = true;
+    while (running == true) 
+    {
+        continue;
+    }
+
     return 0;
 }
